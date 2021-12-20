727dd32da3d4f6c109f1cb666366ece3d5d8fdf8
Quinten <qdderuiter@protonmail.com>
2021-12-20 08:01:08 +0100

    added eraseFunction, rainbow mode, renamed html/css buttons

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 const gridContainer = document.querySelector(".container");
-const eraseButton = document.querySelector(".button");
+const eraseButton = document.querySelector(".erase-button");
+const rainbowButton = document.querySelector(".rainbow-mode-button");
 
 let makeRows = (rows, cols) => {
   gridContainer.style.setProperty("--grid-rows", rows);
@@ -15,9 +16,46 @@ let mouseOverEvent = (e) => {
     if (e.target !== e.currentTarget) {
       e.target.style.backgroundColor = "red";
     }
-    e.stopPropagation;
+    e.stopPropagation();
   });
+};
+
+let eraseFunc = () => {
+  eraseButton.addEventListener("click", () => {
+    eraseColor();
+  });
+};
+
+let eraseColor = () => {
+  gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target !== e.currentTarget) {
+      e.target.style.backgroundColor = "#dbdbdba9";
+    }
+    e.stopPropagation();
+  });
+};
+
+let rainbowFunc = () => {
+  rainbowButton.addEventListener("click", () => {
+    gridContainer.addEventListener("mouseover", (e) => {
+      if (e.target !== e.currentTarget) {
+        e.target.style.backgroundColor = colorGenerator();
+      }
+      e.stopPropagation();
+    });
+  });
+};
+
+let colorGenerator = () => {
+  let makeColorCode = "0123456789ABCDEF";
+  let code = "#";
+  for (let i = 0; i < 6; i++) {
+    code = code + makeColorCode[Math.floor(Math.random() * 16)];
+  }
+  return code;
 };
 
 makeRows(16, 16);
 mouseOverEvent();
+eraseFunc();
+rainbowFunc();
